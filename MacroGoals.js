75a0bb7c4c81277b1714e6bd422b3d731923d6d9
kristian <krispetter@gmail.com>
2026-04-08 10:39:00 +0200

    x

--- MacroGoals.js
+++ MacroGoals.js
@@ -1,7 +1,7 @@
 const age = 27
 const myWeight = 78
 
-const neededCalories = 2500
+const neededCalories = 3100
 const maintainence = Math.round(((10*myWeight) + (6.25*190) - (5*age) + 5) * 1.7) // https://reference.medscape.com/calculator/846/mifflin-st-jeor-equation#
 const neededProteinInGram = myWeight * 1.6
 
