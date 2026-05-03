9e7b3c729e4b2b84af28f6009831249b475eaec1
kristian <krispetter@gmail.com>
2026-05-03 11:44:40 +0200

    x

--- Diet.js
+++ Diet.js
@@ -8,18 +8,17 @@ const dietHistory = [
 	v.gule_epler(100), 
 	v.eldorado_frosne_bringebær(30),
 
-	// kvelds (mealpreppes paa morningen)
+	// kvelds 2 (mealpreppes paa morningen)
 	v.milk(300),
 	v.havregryn(40),
 	v.gresk_yoghurt(100),
 	v.eldorado_frossen_mango(50),
 
-
 	// lunsj og middag (lages sammen)
 	v.first_price_kjøttdeig(400),
 	v.eldorado_frossen_brokkoli(100),
 	v.rå_first_price_ris(200),
-	v.eldorado_ananas_blikkboks(100),
+	v.eldorado_ananas_blikkboks(140), // finn ut om denne er accurate
 
 	// kvelds 1
 	v.gulrotbrød_fra_bakehuset(4*35),
